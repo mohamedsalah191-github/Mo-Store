e5ae9ca722bbf4b1babea30a0c870cbdf983f151
mohamedsalah191-github <mohamedsalah191g@gmail.com>
2024-08-16 18:36:11 +0300

    3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,8 +12,8 @@ import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     {path:"", redirectTo:"home" ,pathMatch:'full'},
-    { path: "home", component: HomeComponent, title:"Home" },
-    { path: "about", component: AboutComponent, title:"About Us" },
+    { path: "home", component: HomeComponent,  },
+    { path: "about", component: AboutComponent,  },
     
     {path:'**',component:NotfoundComponent},
     
